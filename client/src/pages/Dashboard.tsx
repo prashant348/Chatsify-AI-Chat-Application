b7db12657b25ad5ce2a9b60bf07ebb456760b4e1
codingweapon <prashantmistri348@gmail.com>
2025-10-02 10:33:29 +0530

    Changes in Dashboard.tsx

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,22 +1,12 @@
 import { UserButton } from "@clerk/clerk-react";
 
-import { HomeIcon } from "lucide-react";
-import { SettingsIcon } from "lucide-react";
+
 export default function Dashboard() {
 
     return (
         <>
-            <div className="p-[20px]">
-                <h1>Dashboard 📊</h1>
-                <UserButton afterSignOutUrl="/">
-                    <UserButton.UserProfileLink label="Homepage" url="/" labelIcon={<HomeIcon />} />
-                    <UserButton.UserProfilePage label="terms and cond" url="terms" labelIcon={<SettingsIcon />}>
-                        <div>
-                            <h1>Custom Terms Page</h1>
-                            <p>This is the content of the custom terms page.</p>
-                        </div>
-                    </UserButton.UserProfilePage>
-                </UserButton>
+            <div className="h-full w-full">
+                <UserButton />
             </div>
         </>
     )
